cb5b2f2b002389d3f66842d8d88df334a0fabbb0
Alexandra <alyakh2014@gmail.com>
2020-04-04 22:43:29 +0300

    subscribe action

--- widgets/views/subscribe.php
+++ widgets/views/subscribe.php
@@ -9,16 +9,13 @@
 
                 $form = ActiveForm::begin([
                     'id' => 'subscribe_bunner',
-                    'options' => ['class' => 'form-horizontal', 'action'=>'site/subscribe', 'data' => ['pjax' => true]],
+                    'action'=>'/subscribe',
+                    'options' => ['class' => 'form-horizontal', 'data' => ['pjax' => true]],
                 ]);
                 echo $form->field($model, 'email')->input('email', ['placeholder'=> 'Введите email адрес']);
                 echo Html::submitButton('Подписаться', ['class' => 'button gradient-bg']);
                 ActiveForm::end();
                 ?>
-<!--                <form id="subscribe_bunner" >
-                    <input type="email" placeholder="E-mail address">
-                    <input class="button gradient-bg" type="submit" value="Subscribe">
-                </form>-->
             </div>
         </div>
     </div>
