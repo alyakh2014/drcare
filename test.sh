0fa4e4ae8d8547aa0b15d1f37d288ed4b35585b3
Alexandra <alyakh2014@gmail.com>
2020-05-06 21:48:50 +0300

    Some

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #
-# #This is a script that greets the world
-# Usage: ./hello
-#clear
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer.phar
+alias composer='/usr/local/bin/composer.phar'
+composer --version
 php composer install
 php ./vendor/bin/codecept run
