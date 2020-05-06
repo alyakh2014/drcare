aacff108d558dc3b8b387076040ece36668d6dda
Alexandra <alyakh2014@gmail.com>
2020-05-06 23:04:30 +0300

    Prefere dist

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer.phar
-alias composer='/usr/local/bin/composer.phar'
-composer --version
-php composer install
+#curl -sS https://getcomposer.org/installer | php
+#mv composer.phar /usr/local/bin/composer.phar
+#alias composer='/usr/local/bin/composer.phar'
+#composer --version
+#php composer install
 php ./vendor/bin/codecept run
